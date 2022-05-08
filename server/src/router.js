64d559ce0179a16f51a88458d941847c61c2006a
Meelis <73930233+MeelisM@users.noreply.github.com>
2022-05-08 14:24:09 +0300

    added cors

--- server/src/router.js
+++ server/src/router.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const { getExpectedValues, getPlayerStats, getMasteryValues, getMoeValues } = require('./services');
 const { postGraphData, getGraphData } = require('./controllers');
+
+const options = {
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  preflightContinue: false,
+};
+
+router.use(cors(options));
 
 router.get('/test', (req, res) => res.send('Test route'));
 router.get('/expected', getExpectedValues);
