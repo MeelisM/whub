713818332d83e3dc62c30acfbfec3d42e1d7dae2
Meelis <mumm.meelis@gmail.com>
2022-05-24 22:54:02 +0300

    removed scrollbar css

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@
 </template>
 
 <script>
-import SideBar from "@/components/SideBar.vue";
-import { RouterView } from "vue-router";
+import SideBar from '@/components/SideBar.vue';
+import { RouterView } from 'vue-router';
 
 export default {
   components: {
@@ -23,27 +23,10 @@ export default {
 
 <style lang="scss">
 :root {
-  font: 14px "Segoe UI", sans-serif;
+  font: 14px 'Segoe UI', sans-serif;
   --text-dark: #1472ff;
   --text-light: #dedede;
   --bg-light: #1472ff;
-
-  // Firefox scrollbar
-  scrollbar-color: var(--bg-light) var(--bg-dark);
-  scrollbar-width: thin;
-
-  // Other browsers scrollbar
-  body::-webkit-scrollbar {
-    width: 0.5rem;
-  }
-
-  body::-webkit-scrollbar-track {
-    background: var(--bg-light);
-  }
-
-  body::-webkit-scrollbar-thumb {
-    background: var(--bg-dark);
-  }
 }
 
 * {
@@ -115,8 +98,7 @@ body {
   }
 
   .nation-icon {
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
-      rgba(0, 0, 0, 0.22) 0px 15px 12px;
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
     border: solid 1px #484848;
   }
 }
