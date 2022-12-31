4ec566d71b7231e9ae883a425aba1ba903c6a286
Meelis <73930233+MeelisM@users.noreply.github.com>
2022-12-31 14:16:24 +0200

    graph controller

--- server/src/controllers/postGraphData.controller.js
+++ server/src/controllers/postGraphData.controller.js
@@ -1,32 +1,31 @@
-const db = require("../db");
+const db = require('../db');
 const PlayerGraph = db.PlayerGraph;
 
 module.exports = async function (req, res) {
-  try {
-    const user = await PlayerGraph.exists({ playerId: req.body.playerId });
-    if (!user) {
-      await PlayerGraph.create({
-        playerId: req.body.playerId,
-        battles: req.body.battles,
-        winrate: req.body.winrate,
-        wn8: req.body.wn8,
-      });
-      res.status(200).json({ message: "Added!" });
-    } else {
-      await PlayerGraph.updateMany(
-        { playerId: req.body.playerId },
-        {
-          $push: {
-            battles: req.body.battles,
-            winrate: req.body.winrate,
-            wn8: req.body.wn8,
-            $slice: -10,
-          },
+    try {
+        const user = await PlayerGraph.exists({ playerId: req.body.playerId });
+        if (!user) {
+            await PlayerGraph.create({
+                playerId: req.body.playerId,
+                battles: req.body.battles,
+                winrate: req.body.winrate,
+                wn8: req.body.wn8,
+            });
+            res.status(200).json({ message: 'Added!' });
+        } else {
+            await PlayerGraph.updateMany(
+                { playerId: req.body.playerId },
+                {
+                    $push: {
+                        battles: req.body.battles,
+                        winrate: req.body.winrate,
+                        wn8: req.body.wn8,
+                    },
+                }
+            );
+            res.status(200).json({ message: 'Updated!' });
         }
-      );
-      res.status(200).json({ message: "Updated!" });
+    } catch (error) {
+        res.status(500).json({ message: 'Oops! Something went wrong!' });
     }
-  } catch (error) {
-    res.status(500).json({ message: "Oops! Something went wrong!" });
-  }
 };
