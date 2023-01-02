1349bafc96c8f09e79a8d4e0f0266491be08f323
Meelis <73930233+MeelisM@users.noreply.github.com>
2023-01-02 14:13:49 +0200

    linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,16 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-    root: true,
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
-    env: {
-        'vue/setup-compiler-macros': true,
-        node: true,
-    },
-    rules: { 'vue/multi-word-component-names': 'off' },
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  env: {
+    "vue/setup-compiler-macros": true,
+    node: true,
+  },
+  rules: { "vue/multi-word-component-names": "off" },
 };
--- server/src/controllers/postGraphData.controller.js
+++ server/src/controllers/postGraphData.controller.js
@@ -1,11 +1,21 @@
-const db = require('../db');
+const db = require("../db");
 const PlayerGraph = db.PlayerGraph;
 
 module.exports = async function (req, res) {
-    try {
-        const user = await PlayerGraph.updateMany({ playerId: req.body.playerId }, { $push: { winrate: req.body.winrate, battles: req.body.battles, wn8: req.body.wn8 } }, { upsert: true });
-        res.status(200).json({ message: 'Success!' });
-    } catch (error) {
-        res.status(500).json({ message: 'Oops! Something went wrong!' });
-    }
+  try {
+    await PlayerGraph.updateMany(
+      { playerId: req.body.playerId },
+      {
+        $push: {
+          winrate: req.body.winrate,
+          battles: req.body.battles,
+          wn8: req.body.wn8,
+        },
+      },
+      { upsert: true }
+    );
+    res.status(200).json({ message: "Success!" });
+  } catch (error) {
+    res.status(500).json({ message: "Oops! Something went wrong!" });
+  }
 };
