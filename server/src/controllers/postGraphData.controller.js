d7d0d79ec3d285717fd02d676cb0371dee81fd8d
Meelis <mumm.meelis@gmail.com>
2023-01-02 14:12:47 +0200

    playerGraph POST method refactoring

--- server/src/controllers/postGraphData.controller.js
+++ server/src/controllers/postGraphData.controller.js
@@ -3,28 +3,8 @@ const PlayerGraph = db.PlayerGraph;
 
 module.exports = async function (req, res) {
     try {
-        const user = await PlayerGraph.exists({ playerId: req.body.playerId });
-        if (!user) {
-            await PlayerGraph.create({
-                playerId: req.body.playerId,
-                battles: req.body.battles,
-                winrate: req.body.winrate,
-                wn8: req.body.wn8,
-            });
-            res.status(200).json({ message: 'Added!' });
-        } else {
-            await PlayerGraph.updateMany(
-                { playerId: req.body.playerId },
-                {
-                    $push: {
-                        battles: req.body.battles,
-                        winrate: req.body.winrate,
-                        wn8: req.body.wn8,
-                    },
-                }
-            );
-            res.status(200).json({ message: 'Updated!' });
-        }
+        const user = await PlayerGraph.updateMany({ playerId: req.body.playerId }, { $push: { winrate: req.body.winrate, battles: req.body.battles, wn8: req.body.wn8 } }, { upsert: true });
+        res.status(200).json({ message: 'Success!' });
     } catch (error) {
         res.status(500).json({ message: 'Oops! Something went wrong!' });
     }
