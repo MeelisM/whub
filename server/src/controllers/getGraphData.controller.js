6ac3b364e74178c397af11012fef502a4e1c08d7
Meelis <mumm.meelis@gmail.com>
2023-01-02 14:13:49 +0200

    linter

--- server/src/controllers/getGraphData.controller.js
+++ server/src/controllers/getGraphData.controller.js
@@ -1,11 +1,11 @@
-const db = require('../db');
+const db = require("../db");
 const PlayerGraph = db.PlayerGraph;
 
 module.exports = async function (req, res) {
-    try {
-        const data = await PlayerGraph.find({ playerId: req.params.id }).limit(5);
-        res.status(200).json({ data });
-    } catch (error) {
-        res.status(500).json({ message: error });
-    }
+  try {
+    const data = await PlayerGraph.find({ playerId: req.params.id }).limit(5);
+    res.status(200).json({ data });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
