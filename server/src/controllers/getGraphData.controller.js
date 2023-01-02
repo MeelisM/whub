ae587f12bda336b3d05a4f97470dc48350891fe7
Meelis <73930233+MeelisM@users.noreply.github.com>
2023-01-02 14:12:47 +0200

    playerGraph POST method refactoring

--- server/src/controllers/getGraphData.controller.js
+++ server/src/controllers/getGraphData.controller.js
@@ -3,7 +3,7 @@ const PlayerGraph = db.PlayerGraph;
 
 module.exports = async function (req, res) {
     try {
-        const data = await PlayerGraph.find({ playerId: req.params.id }).limit(10);
+        const data = await PlayerGraph.find({ playerId: req.params.id }).limit(5);
         res.status(200).json({ data });
     } catch (error) {
         res.status(500).json({ message: error });
